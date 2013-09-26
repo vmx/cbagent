481e1430dc2e0834b78c7984406d1d8dbec39913
ronniedada <ronniedada@gmail.com>
2013-09-26 15:47:06 -0700

    get disk related columns from atop stats

--- cbagent/collectors/libstats/atopstats.py
+++ cbagent/collectors/libstats/atopstats.py
@@ -1,3 +1,4 @@
+import re
 from uuid import uuid4
 
 from fabric.api import sudo
@@ -30,6 +31,8 @@ class AtopStats(RemoteStats):
         self._cpu_column = self._get_cpu_column()
         self._vsize_column = self._get_vsize_column()
         self._rss_column = self._get_rss_column()
+        self._get_disk_columns()
+        self._disk_flags = self._get_disk_flags()
 
     def restart_atop(self):
         self.stop_atop()
@@ -49,6 +52,29 @@ class AtopStats(RemoteStats):
     def _get_cpu_column(ip):
         output = sudo("atop 1 1 | grep PID")
         return output.split().index("CPU")
+
+    @single_node_task
+    def _get_disk_flags(self):
+        output = sudo("atop -d -f -L200 1 1 | grep 'DSK |'")
+        return map(lambda row: row.split("|")[self._disk_flag_column].strip(),
+                   output.split("\n"))
+
+    @single_node_task
+    def _get_disk_columns(self):
+        output = sudo("atop -d -f -L200 1 1 | grep 'DSK |' | sed -n 1p")
+        cols = output.split("|")
+
+        p = re.compile(".*sd.*|.*dm.*")
+        matches = filter(lambda col: p.match(col), cols)
+        self._disk_flag_column = cols.index(matches[0]) if matches else 1
+
+        p = re.compile(".*KiB/r.*")
+        matches = filter(lambda col: p.match(col), cols)
+        self._disk_read_KB_column = cols.index(matches[0]) if matches else 5
+
+        p = re.compile(".*KiB/w.*")
+        matches = filter(lambda col: p.match(col), cols)
+        self._disk_write_KB_column = cols.index(matches[0]) if matches else 6
 
     @multi_node_task
     def get_process_cpu(self, process):
