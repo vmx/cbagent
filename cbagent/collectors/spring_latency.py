5fe6bbed9a5699328c49f0c85b84b927ef0e4db2
ronniedada <ronniedada@gmail.com>
2013-10-17 16:10:12 -0700

    add couchbase queries for comparison

--- cbagent/collectors/spring_latency.py
+++ cbagent/collectors/spring_latency.py
@@ -3,7 +3,7 @@ from time import time
 from spring.docgen import ExistingKey, NewDocument
 from spring.querygen import NewQuery
 from spring.cbgen import CBGen
-from spring.tuqgen import NewTuq
+from spring.tuqgen import NewTuq, NewCBQuery
 from spring.tuqclient import TuqClient
 
 from cbagent.collectors import Latency
@@ -71,38 +71,44 @@ class SpringQueryLatency(SpringLatency):
 class SpringTuqLatency(SpringLatency):
     COLLECTOR = "spring_tuq_latency"
 
-    METRICS = ("latency_tuq",)
+    METRICS = ("latency_tuq", "latency_query")
 
     def __init__(self, settings, workload, indexes, tuq_server, prefix=None):
         super(Latency, self).__init__(settings)
         self.clients = []
-        bucket = list(self.get_buckets())[0]
-        client = TuqClient(bucket=bucket, host=settings.master_node,
-                           username=settings.rest_username,
-                           password=settings.rest_password,
-                           tuq_server=tuq_server)
-        self.clients.append((bucket, client))
+        self.bucket = list(self.get_buckets())[0]
+        self.tuq_client = TuqClient(bucket=self.bucket, host=settings.master_node,
+                                    username=settings.rest_username,
+                                    password=settings.rest_password,
+                                    tuq_server=tuq_server)
+        self.cb_client = CBGen(bucket=self.bucket, host=settings.master_node,
+                               username=self.bucket, password=settings.rest_password)
 
         self.existing_keys = ExistingKey(workload.working_set,
                                          workload.working_set_access,
                                          prefix=prefix)
         self.new_docs = NewDocument(workload.size)
         self.items = workload.items
-        self.new_tuqs = NewTuq(indexes, bucket)
+        self.new_tuqs = NewTuq(indexes, self.bucket)
+        self.new_queries = NewCBQuery(indexes, self.bucket)
 
-    def measure(self, client, metric):
+    def _measure(self, metric):
         key = self.existing_keys.next(curr_items=self.items, curr_deletes=0)
         doc = self.new_docs.next(key)
         tuq = self.new_tuqs.next(doc)
+        ddoc_name, view_name, query = self.new_queries.next(doc)
 
         t0 = time()
-        client.query(tuq)
+        if metric == "latency_tuq":
+            self.tuq_client.query(tuq)
+        else:
+            self.cb_client.query(ddoc_name, view_name, query=query)
         return 1000 * (time() - t0)
 
     def sample(self):
-        for bucket, client in self.clients:
-            samples = {}
-            for metric in self.METRICS:
-                samples[metric] = self.measure(client, metric)
-            self.store.append(samples, cluster=self.cluster,
-                              bucket=bucket, collector=self.COLLECTOR)
+
+        samples = {}
+        for metric in self.METRICS:
+            samples[metric] = self._measure(metric)
+        self.store.append(samples, cluster=self.cluster,
+                          bucket=self.bucket, collector=self.COLLECTOR)
