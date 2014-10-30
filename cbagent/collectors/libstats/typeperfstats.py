dacf979ae24d5b2e370f55893d63e495b7b39ab1
Venu Uppalapati <venumahesh@gmail.com>
2014-10-30 14:06:42 -0700

    add typeperf stats collection

---
+++ cbagent/collectors/libstats/typeperfstats.py
@@ -0,0 +1,32 @@
+from fabric.api import run
+
+from cbagent.collectors.libstats.remotestats import (
+    RemoteStats, multi_node_task)
+
+
+class TPStats(RemoteStats):
+
+    METRICS = (
+        ("rss", 1),    # already in bytes
+    )
+
+    def __init__(self, hosts, user, password):
+        super(TPStats, self).__init__(hosts, user, password)
+        self.typeperf_cmd = "typeperf\"\\Process(*erl*)\\Working Set\" -sc 1|sed '3q;d'"
+
+    @multi_node_task
+    def get_samples(self, process):
+        samples = {}
+        stdout = run(self.typeperf_cmd)
+        values = stdout.split(',')[1:5]
+        maxrss = 0
+        if stdout:
+            for v in value:
+                v = float(v.replace('"',''))
+            if maxrss<v:
+                maxrss=v
+            metric, multiplier = self.METRICS[0]
+            title = "{}_{}".format(process, metric)
+            samples[title] = float(maxrss) * multiplier
+        else:
+            return samples
